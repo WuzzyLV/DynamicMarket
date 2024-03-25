978a8a28ca37a03b2d57f748f83b72c81fe97083
Wuzzy <wuzzy@github.com>
2024-03-25 20:12:58 +0200

    Start of items cfg

---
+++ src/main/java/me/wuzzyxy/dynamicmarket/configs/ItemConfig.java
@@ -0,0 +1,12 @@
+package me.wuzzyxy.dynamicmarket.configs;
+
+import me.wuzzyxy.dynamicmarket.DynamicMarket;
+
+public class ItemConfig {
+    DynamicMarket plugin;
+    public ItemConfig(DynamicMarket plugin) {
+        this.plugin = plugin;
+
+
+    }
+}

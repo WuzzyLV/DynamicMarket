9a1aaebcd8816e4c6301ca14a3e1d4d032be71d3
Wuzzy <wuzzy@github.com>
2024-03-28 08:06:46 +0200

    Item Config implementation

--- src/main/java/me/wuzzyxy/dynamicmarket/configs/ItemConfig.java
+++ src/main/java/me/wuzzyxy/dynamicmarket/configs/ItemConfig.java
@@ -1,12 +1,45 @@
 package me.wuzzyxy.dynamicmarket.configs;
 
 import me.wuzzyxy.dynamicmarket.DynamicMarket;
+import me.wuzzyxy.dynamicmarket.items.MarketItem;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemConfig {
+    FileConfiguration config;
     DynamicMarket plugin;
     public ItemConfig(DynamicMarket plugin) {
         this.plugin = plugin;
 
+        config = initFiles();
 
+//        List<MarketItem> items = getAllItems();
+//        for (MarketItem item : items) {
+//            plugin.getLogger().info("Loaded item: " + item.toString());
+//        }
+    }
+
+    public YamlConfiguration initFiles() {
+        File configFile = new File(plugin.getDataFolder(), "items.yml");
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdirs();
+            plugin.saveResource("items.yml", false);
+        }
+
+        return YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    public List<MarketItem> getAllItems() {
+        List<MarketItem> items = new ArrayList<>();
+        for (String key : config.getConfigurationSection("items").getKeys(false)) {
+            double basePrice = config.getDouble("items." + key + ".base_price");
+            double minPrice = config.getDouble("items." + key + ".min_price");
+            items.add(new MarketItem(key, basePrice, 0,0, minPrice));
+        }
+        return items;
     }
 }

7ed7ea0631fb05dc6b64fc29bdd07a2c154cfaba
Wuzzy <wuzzy@github.com>
2024-03-28 08:05:34 +0200

    Add push interval to config object

--- src/main/java/me/wuzzyxy/dynamicmarket/configs/PluginConfig.java
+++ src/main/java/me/wuzzyxy/dynamicmarket/configs/PluginConfig.java
@@ -13,9 +13,11 @@ public class PluginConfig {
         USERNAME= plugin.getConfig().getString("mysql.username");
         PASSWORD= plugin.getConfig().getString("mysql.password");
 
+        PUSH_INTERVAL = plugin.getConfig().getInt("push_interval");
 
     }
 
+    public final int PUSH_INTERVAL;
     public final String HOST;
     public final int PORT;
     public final String DATABASE;

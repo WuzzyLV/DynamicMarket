7544691afd00e0a046c9210471ee3da30533d1da
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-04-07 17:29:59 +0300

    Placeholders for price

---
+++ src/main/java/me/wuzzyxy/dynamicmarket/placeholders/SellPriceExpansion.java
@@ -0,0 +1,69 @@
+package me.wuzzyxy.dynamicmarket.placeholders;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.wuzzyxy.dynamicmarket.DynamicMarket;
+import me.wuzzyxy.dynamicmarket.items.MarketItem;
+import me.wuzzyxy.dynamicmarket.market.MarketManager;
+import me.wuzzyxy.dynamicmarket.market.PriceHandler;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DecimalFormat;
+
+public class SellPriceExpansion extends PlaceholderExpansion {
+    private final DynamicMarket plugin;
+    private final MarketManager manager;
+    private final PriceHandler priceHandler;
+    DecimalFormat df;
+    public SellPriceExpansion(DynamicMarket plugin, MarketManager manager, PriceHandler priceHandler) {
+        this.plugin = plugin;
+        this.manager = manager;
+        this.priceHandler = priceHandler;
+
+        df = new DecimalFormat("0.00");
+    }
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return "DMSellPrice";
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return "Wuzzy";
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return plugin.getDescription().getVersion();
+    }
+    @Override
+    public boolean persist() {
+        return true; //
+    }
+
+    public String onRequest(OfflinePlayer player, @NotNull String params) {
+        String[] args = params.split(",");
+
+        if (!(args.length == 2 || args.length == 3)) return "DMSellPrice_Item,Amount or DMSellPrice_Item,Amount,Percentage " + args.length + " args provided";
+
+        MarketItem item = manager.getPersistedItem(args[0]).orElse(null);
+        if (item == null) return "Item not found";
+
+        try {
+            int amount = Integer.parseInt(args[1]);
+            double price;
+            if (args.length == 3) {
+                double percentage = Double.parseDouble(args[2]);
+                price = priceHandler.getSellPrice(item, amount, percentage);
+            }else {
+                price = priceHandler.getSellPrice(item, amount);
+            }
+            return df.format(price);
+        } catch (NumberFormatException e) {
+            return "Invalid params";
+        }
+    }
+
+
+}

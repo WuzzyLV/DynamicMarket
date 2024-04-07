8cda071543dd72bc4b5acec917273bdd0f8a7e8b
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-04-07 17:30:08 +0300

    Creating a price handler

--- src/main/java/me/wuzzyxy/dynamicmarket/market/PriceHandler.java
+++ src/main/java/me/wuzzyxy/dynamicmarket/market/PriceHandler.java
@@ -11,14 +11,24 @@ public class PriceHandler {
         percentage = pluginConfig.PERCENTAGE;
     }
 
-    public double getPrice(MarketItem item, int amount) {
+    public double getBuyPrice(MarketItem item, int amount) {
+        double price = ((item.getBasePrice() * item.getPercentage()) * item.getBoughtAmount() -
+                ((item.getBasePrice() * item.getPercentage()) * item.getSoldAmount()) + item.getBasePrice()) * amount;
+        return Math.max(price, item.getMinPrice());
+    }
+    public double getBuyPrice(MarketItem item, int amount, double percentage) {
         double price = ((item.getBasePrice() * percentage) * item.getBoughtAmount() -
                 ((item.getBasePrice() * percentage) * item.getSoldAmount()) + item.getBasePrice()) * amount;
         return Math.max(price, item.getMinPrice());
     }
-    public double getPrice(MarketItem item, int amount, double percentage) {
-        double price = ((item.getBasePrice() * percentage) * item.getBoughtAmount() -
-                ((item.getBasePrice() * percentage) * item.getSoldAmount()) + item.getBasePrice()) * amount;
+
+    public double getSellPrice(MarketItem item, int amount) {
+        double price = getBuyPrice(item, amount) * pluginConfig.SELL_MULTIPLIER;
+        return Math.max(price, item.getMinPrice());
+    }
+
+    public double getSellPrice(MarketItem item, int amount, double percentage) {
+        double price = getBuyPrice(item, amount, percentage) * pluginConfig.SELL_MULTIPLIER;
         return Math.max(price, item.getMinPrice());
     }
 }

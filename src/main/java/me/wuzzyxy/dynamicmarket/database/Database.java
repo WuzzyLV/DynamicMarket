7a9735f0fa78b60e9105dfa63252071c7d0a47bf
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-04-07 17:31:30 +0300

    Update database interface bcs of percentage

--- src/main/java/me/wuzzyxy/dynamicmarket/database/Database.java
+++ src/main/java/me/wuzzyxy/dynamicmarket/database/Database.java
@@ -7,10 +7,10 @@ import java.util.List;
 public interface Database {
     void die();
 
-    MarketItem addItem(String item, double basePrice, double minPrice);
-    MarketItem addItem(String item, double basePrice, double minPrice, int boughtAmount, int soldAmount);
-    MarketItem setItem(String item, double basePrice, double minPrice, int boughtAmount, int soldAmount);
-    MarketItem setItemPrices(String item, double basePrice, double minPrice);
+    MarketItem addItem(String item, double basePrice, double minPrice, double percentage);
+    MarketItem addItem(String item, double basePrice, double minPrice, int boughtAmount, int soldAmount, double percentage);
+    MarketItem setItem(String item, double basePrice, double minPrice, int boughtAmount, int soldAmount, double percentage);
+    MarketItem setItemStatics(String item, double basePrice, double minPrice, double percentage);
     boolean removeItem(String item);
     MarketItem getItem(String item);
     List<MarketItem> getAllItems();
@@ -28,6 +28,7 @@ public interface Database {
     MarketItem setSoldAmount(MarketItem item, int amount);
     MarketItem setAmounts(MarketItem item, int boughtAmount, int soldAmount);
 
+    @Deprecated
     boolean createHistoryPoint(MarketItem item);
 
 

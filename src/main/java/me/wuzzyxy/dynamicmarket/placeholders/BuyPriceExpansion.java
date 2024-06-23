9ec385c8086658598891fefa5d0c3cea639af4b1
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-06-23 15:27:53 +0300

    Changing something idk

--- src/main/java/me/wuzzyxy/dynamicmarket/placeholders/BuyPriceExpansion.java
+++ src/main/java/me/wuzzyxy/dynamicmarket/placeholders/BuyPriceExpansion.java
@@ -46,20 +46,14 @@ public class BuyPriceExpansion extends PlaceholderExpansion {
     public String onRequest(OfflinePlayer player, @NotNull String params) {
         String[] args = params.split(",");
 
-        if (!(args.length == 2 || args.length == 3)) return "DMBuyPrice_Item,Amount or DMBuyPrice_Item,Amount,Percentage " + args.length + " args provided";
+        if (args.length != 2) return "DMBuyPrice_Item,Amount" + args.length + " args provided";
 
         MarketItem item = manager.getPersistedItem(args[0]).orElse(null);
-        if (item == null) return "Item not found";
+        if (item == null) return args[0]+ " not found";
 
         try {
             int amount = Integer.parseInt(args[1]);
-            double price;
-            if (args.length == 3) {
-                double percentage = Double.parseDouble(args[2]);
-                price = priceHandler.getBuyPrice(item, amount, percentage);
-            }else {
-                price = priceHandler.getBuyPrice(item, amount);
-            }
+            double price = priceHandler.getBuyPrice(item, amount);
             return df.format(price);
         } catch (NumberFormatException e) {
             return "Invalid params";
--- src/main/java/me/wuzzyxy/dynamicmarket/placeholders/SellPriceExpansion.java
+++ src/main/java/me/wuzzyxy/dynamicmarket/placeholders/SellPriceExpansion.java
@@ -45,20 +45,14 @@ public class SellPriceExpansion extends PlaceholderExpansion {
     public String onRequest(OfflinePlayer player, @NotNull String params) {
         String[] args = params.split(",");
 
-        if (!(args.length == 2 || args.length == 3)) return "DMSellPrice_Item,Amount or DMSellPrice_Item,Amount,Percentage " + args.length + " args provided";
+        if (args.length != 2) return "DMSellPrice_Item,Amount" + args.length + " args provided";
 
         MarketItem item = manager.getPersistedItem(args[0]).orElse(null);
-        if (item == null) return "Item not found";
+        if (item == null) return args[0]+ " not found";
 
         try {
             int amount = Integer.parseInt(args[1]);
-            double price;
-            if (args.length == 3) {
-                double percentage = Double.parseDouble(args[2]);
-                price = priceHandler.getSellPrice(item, amount, percentage);
-            }else {
-                price = priceHandler.getSellPrice(item, amount);
-            }
+            double price = priceHandler.getSellPrice(item, amount);
             return df.format(price);
         } catch (NumberFormatException e) {
             return "Invalid params";

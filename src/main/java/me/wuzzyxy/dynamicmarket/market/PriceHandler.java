5785d14150a9ab32670c86f64c7064d454a8ec47
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-06-23 15:28:11 +0300

    Changing something idk

--- src/main/java/me/wuzzyxy/dynamicmarket/market/PriceHandler.java
+++ src/main/java/me/wuzzyxy/dynamicmarket/market/PriceHandler.java
@@ -14,19 +14,10 @@ public class PriceHandler {
                 ((item.getBasePrice() * item.getPercentage()) * item.getSoldAmount()) + item.getBasePrice()) * amount;
         return Math.max(price, item.getMinPrice());
     }
-    public double getBuyPrice(MarketItem item, int amount, double percentage) {
-        double price = ((item.getBasePrice() * percentage) * item.getBoughtAmount() -
-                ((item.getBasePrice() * percentage) * item.getSoldAmount()) + item.getBasePrice()) * amount;
-        return Math.max(price, item.getMinPrice());
-    }
 
     public double getSellPrice(MarketItem item, int amount) {
         double price = getBuyPrice(item, amount) * pluginConfig.SELL_MULTIPLIER;
         return Math.max(price, item.getMinPrice());
     }
 
-    public double getSellPrice(MarketItem item, int amount, double percentage) {
-        double price = getBuyPrice(item, amount, percentage) * pluginConfig.SELL_MULTIPLIER;
-        return Math.max(price, item.getMinPrice());
-    }
 }
